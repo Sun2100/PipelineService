8e0db1eb0f7c9897b7380b1a3a2fd32028d6b881
Supadejh Phatpanichot <sun@Supadejhs-MacBook-Air.local>
2024-05-22 21:13:22 -0400

    finished setting up development environment

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # https://python-poetry.org/docs/
 
 # New line of code added
+
+# https://airflow.apache.org/docs/apache-airflow/stable/installation/installing-from-pypi.html
+# https://docs.dagster.io/getting-started/install
+
+poetry add dagster dagster-webserver --find-links=https://github.com/dagster-io/build-grpcio/wiki/Wheels
+
+poetry source add grpcio https://github.com/dagster-io/build-grpcio/wiki/Wheels
+
+
+https://github.com/dagster-io/dagster/issues/15699
